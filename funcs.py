cfff5aff981500f3f8ccf3bdc4d938da2f2aaa20
Joachim Lehmann <joachim.lehmann@googlemail.com>
2024-01-02 21:31:06 +0100

    Reformatted and removed unnecessary functions.

--- funcs.py
+++ funcs.py
@@ -7,21 +7,6 @@ def getutc():
     return datetime.utcnow()
 
 
-def write_db(statement: str) -> None:
-    """Accepts and executes SQL Statements
-    :param statement:
-    :return:
-    """
-    connection = mariadb.connect(
-        host=db_host, user=db_user, password=db_pass, database=db_name
-    )
-
-    cursor = connection.cursor()
-    cursor.execute(statement)
-    connection.commit()
-    return None
-
-
 def write_energydb(table, con) -> None:
     """Accepts and executes SQL Statements
     :param statement:

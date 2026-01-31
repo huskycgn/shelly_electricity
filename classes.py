cd0c8da6cd3786b935886b8daceaa5cb927ed31a
huskycgn <joachim.lehmann@googlemail.com>
2026-01-31 01:11:38 +0100

    improved error handling

--- classes.py
+++ classes.py
@@ -10,7 +10,10 @@ class Room:
         self.consumption = self.get_shelly_lan()
 
     def get_shelly_lan(self):
-        base_url = f"http://{self.ipaddress}/rpc/Switch.GetStatus?id=0"
-        response = requests.get(url=base_url)
-        json_data = response.json()
+        try:
+            base_url = f"http://{self.ipaddress}/rpc/Switch.GetStatus?id=0"
+            response = requests.get(url=base_url)
+            json_data = response.json()
+        except:
+            return 0
         return json_data["apower"]
